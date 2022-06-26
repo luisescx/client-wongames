6be9f590ddba865653dafd6b024354f77b142d94
luisescx <luis.esculapio@Live.com>
2022-06-26 14:00:23 -0300

    Initial commit from Create Next App

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require("next-pwa");
+const isProd = process.env.NODE_ENV === "production";
+
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    disable: !isProd
+  },
+  nextConfig: {
+    reactStrictMode: true
+  }
+});
