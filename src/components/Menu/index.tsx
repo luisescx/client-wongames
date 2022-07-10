861c1ebe5ef0e136d95aa22631462dddada288c9
luisescx <luis.esculapio@live.com>
2022-07-10 20:57:42 -0300

    chore: menu component

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,30 +1,84 @@
 import { Menu as MenuIcon } from "@styled-icons/material-outlined";
 import { ShoppingCart as ShoppingCartIcon } from "@styled-icons/material-outlined";
 import { Search as SearchIcon } from "@styled-icons/material-outlined";
-import Logo from "../Logo";
-
+import { useState } from "react";
+import { Close as CloseIcon } from "@styled-icons/material-outlined/Close";
+import Button from "components/Button";
+import MediaMatch from "components/MediaMatch";
+import Logo from "components//Logo";
 import * as S from "./styles";
 
-const Menu = () => (
-  <S.Wrapper>
-    <S.IconWrapper>
-      <MenuIcon aria-label="Open Menu" />
-    </S.IconWrapper>
+export type MenuProps = {
+  username?: string;
+};
 
-    <S.LogoWrapper>
-      <Logo hideOnMobile />
-    </S.LogoWrapper>
+const Menu = ({ username }: MenuProps) => {
+  const [isOpen, setIsOpen] = useState(false);
 
-    <S.MenuGroup>
-      <S.IconWrapper>
-        <ShoppingCartIcon aria-label="Open shopping cart" />
-      </S.IconWrapper>
+  return (
+    <S.Wrapper>
+      <MediaMatch lessThan="medium">
+        <S.IconWrapper onClick={() => setIsOpen(true)}>
+          <MenuIcon aria-label="Open Menu" />
+        </S.IconWrapper>
+      </MediaMatch>
 
-      <S.IconWrapper>
-        <SearchIcon aria-label="search" />
-      </S.IconWrapper>
-    </S.MenuGroup>
-  </S.Wrapper>
-);
+      <S.LogoWrapper>
+        <Logo hideOnMobile />
+      </S.LogoWrapper>
+
+      <MediaMatch greaterThan="medium">
+        <S.MenuNav>
+          <S.MenuLink href="#">Home</S.MenuLink>
+          <S.MenuLink href="#">Explore</S.MenuLink>
+        </S.MenuNav>
+      </MediaMatch>
+
+      <S.MenuGroup>
+        <S.IconWrapper>
+          <ShoppingCartIcon aria-label="Open shopping cart" />
+        </S.IconWrapper>
+
+        <S.IconWrapper>
+          <SearchIcon aria-label="search" />
+        </S.IconWrapper>
+
+        {!username && (
+          <MediaMatch greaterThan="medium">
+            <Button>Sign in</Button>
+          </MediaMatch>
+        )}
+      </S.MenuGroup>
+
+      <S.MenuFull aria-hidden={!isOpen} isOpen={isOpen}>
+        <CloseIcon aria-label="Close Menu" onClick={() => setIsOpen(false)} />
+
+        <S.MenuNav>
+          <S.MenuLink href="#">Home</S.MenuLink>
+          <S.MenuLink href="#">Explore</S.MenuLink>
+
+          {!!username && (
+            <>
+              <S.MenuLink href="#">My account</S.MenuLink>
+              <S.MenuLink href="#">Wishlist</S.MenuLink>
+            </>
+          )}
+        </S.MenuNav>
+
+        {!username && (
+          <S.RegisterBox>
+            <Button fullWidth size="large">
+              Log in now
+            </Button>
+            <span>or</span>
+            <S.CreateAccount href="#" title="Sign Up">
+              Sign Up
+            </S.CreateAccount>
+          </S.RegisterBox>
+        )}
+      </S.MenuFull>
+    </S.Wrapper>
+  );
+};
 
 export default Menu;
