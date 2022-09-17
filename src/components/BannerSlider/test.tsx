1772fefa8706f6ba54df639e97a631f95206e5e9
luisescx <luis.esculapio@live.com>
2022-09-17 17:42:09 -0300

    chore: bannerSlide component

--- src/components/BannerSlider/test.tsx
+++ src/components/BannerSlider/test.tsx
@@ -42,5 +42,11 @@ describe("<BannerSlider />", () => {
     expect(
       screen.getByRole("heading", { name: /defy death 2/i, hidden: true })
     ).toBeInTheDocument();
+
+    it("should render with the dots", () => {
+      const { container } = renderWithTheme(<BannerSlider items={items} />);
+
+      expect(container.querySelector(".slick-dots")).toBeInTheDocument();
+    });
   });
 });
