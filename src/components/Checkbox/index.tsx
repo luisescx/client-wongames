5c6e626c826a6fd505349ee1d1df62c47cb88e76
luisescx <luis.esculapio@live.com>
2022-09-30 21:21:16 -0300

    .

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -1,25 +1,46 @@
+import { InputHTMLAttributes, useCallback, useState } from "react";
 import * as S from "./styles";
 
 export type CheckboxProps = {
+  onCheck?: (status: boolean) => void;
   label?: string;
   labelFor?: string;
   labelColor?: "white" | "black";
-};
+} & InputHTMLAttributes<HTMLInputElement>;
 
 const Checkbox = ({
   label,
   labelFor = "",
-  labelColor = "white"
-}: CheckboxProps) => (
-  <S.Wrapper>
-    <S.Input id={labelFor} type="checkbox" />
+  labelColor = "white",
+  onCheck
+}: CheckboxProps) => {
+  const [checked, setChecked] = useState(false);
 
-    {!!label && (
-      <S.Label htmlFor={labelFor} labelColor={labelColor}>
-        {label}
-      </S.Label>
-    )}
-  </S.Wrapper>
-);
+  const onChange = useCallback(() => {
+    const status = !checked;
+    setChecked(status);
+
+    if (onCheck) {
+      onCheck(status);
+    }
+  }, [checked, onCheck]);
+
+  return (
+    <S.Wrapper>
+      <S.Input
+        id={labelFor}
+        type="checkbox"
+        onChange={onChange}
+        checked={checked}
+      />
+
+      {!!label && (
+        <S.Label htmlFor={labelFor} labelColor={labelColor}>
+          {label}
+        </S.Label>
+      )}
+    </S.Wrapper>
+  );
+};
 
 export default Checkbox;
