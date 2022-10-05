1c696cd9b21b1e8b4c4debd4e5b8b37283371e72
luisescx <luis.esculapio@live.com>
2022-10-04 21:21:19 -0300

    chore: finish checkbox component

--- src/components/Checkbox/stories.tsx
+++ src/components/Checkbox/stories.tsx
@@ -4,9 +4,46 @@ import Checkbox, { CheckboxProps } from ".";
 export default {
   title: "Checkbox",
   component: Checkbox,
-  argTypes: {
-    onCheck: { action: "checked" }
+  parameters: {
+    layout: "fullscreen",
+    backgrounds: {
+      default: "won-dark"
+    }
+  },
+  args: {
+    isChecked: false,
+    labelColor: "white"
   }
-} as Meta;
+} as Meta<CheckboxProps>;
 
-export const Default: Story<CheckboxProps> = (args) => <Checkbox {...args} />;
+export const Default: Story<CheckboxProps> = (args) => (
+  <>
+    <div style={{ padding: 10 }}>
+      <Checkbox
+        name="category"
+        label="Action"
+        labelFor="action"
+        isChecked
+        {...args}
+      />
+    </div>
+
+    <div style={{ padding: 10 }}>
+      <Checkbox
+        name="category"
+        label="Adventure"
+        labelFor="adventure"
+        {...args}
+      />
+    </div>
+
+    <div style={{ padding: 10 }}>
+      <Checkbox
+        name="category"
+        label="Strategy"
+        labelFor="strategy"
+        {...args}
+      />
+    </div>
+  </>
+);
--- src/components/Checkbox/test.tsx
+++ src/components/Checkbox/test.tsx
@@ -6,13 +6,17 @@ import Checkbox from ".";
 
 describe("<Checkbox />", () => {
   it("should render with label", () => {
-    renderWithTheme(<Checkbox label="checkbox label" labelFor="check" />);
+    const { container } = renderWithTheme(
+      <Checkbox label="checkbox label" labelFor="check" />
+    );
 
     expect(screen.getByRole("checkbox")).toBeInTheDocument();
 
     expect(screen.getByLabelText(/checkbox label/i)).toBeInTheDocument();
 
     expect(screen.getByText(/checkbox label/i)).toHaveAttribute("for", "check");
+
+    expect(container.firstChild).toMatchSnapshot();
   });
 
   it("should render without label", () => {
@@ -54,5 +58,17 @@ describe("<Checkbox />", () => {
       expect(onCheck).toHaveBeenCalledTimes(1);
     });
     expect(onCheck).toHaveBeenCalledWith(false);
+  });
+
+  it("should be accessible with tab", async () => {
+    renderWithTheme(<Checkbox label="Checkbox" labelFor="Checkbox" />);
+
+    expect(document.body).toHaveFocus();
+
+    userEvent.tab();
+
+    await waitFor(() => {
+      expect(screen.getByLabelText(/checkbox/i)).toHaveFocus();
+    });
   });
 });
