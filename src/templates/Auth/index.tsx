5d1393354ea1a0fd30b2b1eae3f5f0a0df154553
luisescx <luis.esculapio@live.com>
2022-10-09 21:14:51 -0300

    chore: add new styles to Auth template

--- src/templates/Auth/index.tsx
+++ src/templates/Auth/index.tsx
@@ -10,23 +10,30 @@ type AuthProps = {
 const Auth = ({ title, children }: AuthProps) => (
   <S.Wrapper>
     <S.BannerBlock>
-      <Logo />
+      <S.BannerContent>
+        <Logo />
 
-      <Heading>All your favorite games in one place</Heading>
+        <div>
+          <Heading>All your favorite games in one place</Heading>
+          <S.Subtitle>
+            <strong>WON</strong> is the best and most complete gaming platform.
+          </S.Subtitle>
+        </div>
 
-      <S.Subtitle>
-        <strong>WON</strong> is the best and most complete gaming platform.
-      </S.Subtitle>
-
-      <S.Footer>Won Games 2022 © Todos os Direitos Reservados</S.Footer>
+        <S.Footer>Won Games 2022 © Todos os Direitos Reservados</S.Footer>
+      </S.BannerContent>
     </S.BannerBlock>
 
     <S.Content>
-      <Logo color="black" size="large" />
+      <S.ContentWrapper>
+        <Logo color="black" size="large" />
 
-      <Heading>{title}</Heading>
+        <Heading color="black" lineColor="secondary" lineLeft>
+          {title}
+        </Heading>
 
-      {children}
+        {children}
+      </S.ContentWrapper>
     </S.Content>
   </S.Wrapper>
 );
