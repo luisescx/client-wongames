c6b6f1536f23671f4a31eaab0ea321e88f98387d
luisescx <luis.esculapio@live.com>
2022-10-12 19:07:58 -0300

    feat: add GameInfo Component

---
+++ src/components/GameInfo/test.tsx
@@ -0,0 +1,35 @@
+import { screen } from "@testing-library/react";
+import { renderWithTheme } from "utils/tests/helpers";
+
+import GameInfo from ".";
+
+const props = {
+  title: "My Game Title",
+  description: "Game Description",
+  price: "210,00"
+};
+
+describe("<GameInfo />", () => {
+  it("should render the game information", () => {
+    renderWithTheme(<GameInfo {...props} />);
+
+    expect(
+      screen.getByRole("heading", { name: props.title })
+    ).toBeInTheDocument();
+
+    expect(screen.getByText(/\$210,00/)).toBeInTheDocument();
+
+    expect(screen.getByText(props.description)).toBeInTheDocument();
+  });
+
+  it("should render buttons", () => {
+    renderWithTheme(<GameInfo {...props} />);
+
+    expect(
+      screen.getByRole("button", { name: /add to cart/i })
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole("button", { name: /wishlist/i })
+    ).toBeInTheDocument();
+  });
+});
