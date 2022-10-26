c08bfb5828678e47f762fd63cd429d419e3dbb12
luisescx <luis.esculapio@live.com>
2022-10-25 23:36:10 -0300

    feat: add Profile component

--- src/templates/Profile/index.tsx
+++ src/templates/Profile/index.tsx
@@ -2,6 +2,7 @@ import { Container } from "components/Container";
 import Heading from "components/Heading";
 import ProfileMenu from "components/ProfileMenu";
 import Base from "templates/Base";
+import { useRouter } from "next/router";
 
 import * as S from "./styles";
 
@@ -9,19 +10,23 @@ export type ProfileTemplateProps = {
   children: React.ReactNode;
 };
 
-const Profile = ({ children }: ProfileTemplateProps) => (
-  <Base>
-    <Container>
-      <Heading lineLeft lineColor="secondary">
-        My profile
-      </Heading>
+const Profile = ({ children }: ProfileTemplateProps) => {
+  const { asPath } = useRouter();
 
-      <S.Main>
-        <ProfileMenu />
-        <S.Content>{children}</S.Content>
-      </S.Main>
-    </Container>
-  </Base>
-);
+  return (
+    <Base>
+      <Container>
+        <Heading lineLeft lineColor="secondary">
+          My profile
+        </Heading>
+
+        <S.Main>
+          <ProfileMenu activeLink={asPath} />
+          <S.Content>{children}</S.Content>
+        </S.Main>
+      </Container>
+    </Base>
+  );
+};
 
 export default Profile;
