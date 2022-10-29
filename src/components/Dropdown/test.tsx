fa532aaa55c1db131188402ff08220ca56a924ab
luisescx <luis.esculapio@live.com>
2022-10-29 18:48:18 -0300

    feat: add UserDropdown component

--- src/components/Dropdown/test.tsx
+++ src/components/Dropdown/test.tsx
@@ -6,7 +6,7 @@ import Dropdown from ".";
 
 describe("<Dropdown />", () => {
   beforeEach(() => {
-    const title = <h1 aria-label="toogle dropdown">Click here</h1>;
+    const title = <h1 aria-label="toggle dropdown">Click here</h1>;
 
     renderWithTheme(
       <Dropdown title={title}>
@@ -16,7 +16,7 @@ describe("<Dropdown />", () => {
   });
 
   it("should render title", () => {
-    expect(screen.getByLabelText(/toogle dropdown/)).toBeInTheDocument();
+    expect(screen.getByLabelText(/toggle dropdown/)).toBeInTheDocument();
   });
 
   it("should handle open/close dropdown", async () => {
@@ -25,7 +25,7 @@ describe("<Dropdown />", () => {
     expect(content).toHaveStyle({ opacity: 0 });
     expect(content.getAttribute("aria-hidden")).toBe("true");
 
-    userEvent.click(screen.getByLabelText(/toogle dropdown/));
+    userEvent.click(screen.getByLabelText(/toggle dropdown/));
 
     await waitFor(() => {
       expect(content).toHaveStyle({ opacity: 1 });
---
+++ src/components/UserDropdown/test.tsx
@@ -0,0 +1,31 @@
+import { screen, waitFor } from "@testing-library/react";
+import { renderWithTheme } from "utils/tests/helpers";
+import userEvent from "@testing-library/user-event";
+
+import UserDropdown from ".";
+
+describe("<UserDropdown />", () => {
+  it("should render the heading", () => {
+    renderWithTheme(<UserDropdown username="John" />);
+
+    expect(screen.getByText(/john/i)).toBeInTheDocument();
+  });
+
+  it("should render the menu", async () => {
+    renderWithTheme(<UserDropdown username="John" />);
+
+    userEvent.click(screen.getByText(/john/i));
+
+    await waitFor(() => {
+      expect(
+        screen.getByRole("link", { name: /my profile/i })
+      ).toBeInTheDocument();
+      expect(
+        screen.getByRole("link", { name: /wishlist/i })
+      ).toBeInTheDocument();
+      expect(
+        screen.getByRole("link", { name: /sign out/i })
+      ).toBeInTheDocument();
+    });
+  });
+});
