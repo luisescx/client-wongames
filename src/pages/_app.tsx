87ebfb5994c9e4d4bd333544fcd65fec2e2ed457
luisescx <luis.esculapio@live.com>
2022-11-18 19:40:50 -0300

    feat: add nextjs progressbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { ApolloProvider } from "@apollo/client";
+import NextNprogress from "nextjs-progressbar";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import { ThemeProvider } from "styled-components";
@@ -26,6 +27,12 @@ function App({ Component, pageProps }: AppProps) {
           </Head>
 
           <GlobalStyles />
+          <NextNprogress
+            color="#F231A5"
+            startPosition={0.3}
+            stopDelayMs={200}
+            height={5}
+          />
           <Component {...pageProps} />
         </CartProvider>
       </ThemeProvider>
